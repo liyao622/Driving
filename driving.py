a7f00d5e6991f6d34898edc273828506f0b851e9
Leo Wu <leo0622@hotmail.com.tw>
2019-08-01 22:01:40 +0800

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,12 @@
+country = input('請問你是哪國人: ')
+age = input('請輸入年齡: ')
+if country == '台灣':
+	if int(age) >= 18:
+		print('你可以考駕照')
+	else:
+		print('你還不能考駕照')
+elif country == '美國':
+	if int(age) >= 16:
+		print('你可以考駕照')
+	else:
+		print('你還不能考駕照')
